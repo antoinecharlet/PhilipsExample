5627906d2b1c88cda65b440ea9f796aafc82d329
Uli Hansen <ulrich.hansen@acentic.com>
2019-02-05 14:14:52 +0100

    changes on Philips Example 8, WebService

--- Example_08_TVDiscovery_Finished/web.php
+++ Example_08_TVDiscovery_Finished/web.php
@@ -1,26 +1,34 @@
 <?php
     $curfiledir = getcwd();
-   	$myDate =  date_default_timezone_set('Europe/Brussels');
-	
-	//log to file
+    date_default_timezone_set('Europe/Brussels');
+    $timestamp=time();
+    $myDate= date("d.m.Y - H:i", $timestamp);
+        
+    // log to file
     $myFile = "targetreg.txt";
     $fh = fopen($myFile, 'a') or die("can't open file");
     $data_back = file_get_contents('php://input');
     $json_back_clean2 = preg_replace('/[\x00-\x1F\x80-\xFF]/','',$data_back);
     $json_back_clean = preg_replace('/^jsonData=/','',$json_back_clean2);
 
-    fwrite($fh, "Date: " .$myDate." ******* JSONresponse: ".$json_back_clean."\n");
+    $json_format=json_encode($json_back_clean, JSON_PRETTY_PRINT);
+
+    //fwrite($fh, "\nDate: " .$myDate." ******* JSONresponse: ".$json_back_clean."\n");
+    //fwrite($fh, "\nDate: " .$myDate." ******* JSONresponse: ".$json_format."\n");
 
     // parse JSON object
     try {
         
         $json_data  = json_decode($json_back_clean,true);
         
+        fwrite($fh, "\nDate: " .$myDate." ******* \033[31m".$json_data['Fun']."\033[0m\n");
+        fwrite($fh, "\t\t".$json_back_clean."\n");
 
         // manage tv discovery
         if ($json_data['Fun'] == 'TVDiscoveryService') {
 
-        	// fwrite($fh, "This is if statement \n");
+
+            fwrite($fh, "This is if statement \n");
 
             $pollfreq   = $json_data['CommandDetails']['WebServiceParameters']['PollingFrequency'];
             $uniqueid   = $json_data['CommandDetails']['WebServiceParameters']['TVUniqueID'];
@@ -32,7 +40,7 @@
             $vsecureid  = $json_data['CommandDetails']['TVDiscoveryParameters']['VSecureTVID'];
 
             $html = new DOMDocument(); 
-            $html->loadHTMLFile('index.html'); 
+            $html->loadHTMLFile('template.html'); 
             $html->getElementById('ip')->nodeValue = "IP address is: $ipaddress";
             $html->saveHTMLFile("index.html");
 
@@ -44,7 +52,7 @@
             else {
                 $pwstatus = "StandBy";
             }	
-        }
+	}
         // end TV discovery
     }
     catch (Exception $e) {
