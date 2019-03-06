d201730e15b5a5a1d79b82782679aafbd5902307
Uli Hansen <ulrich.hansen@acentic.com>
2019-03-06 15:44:21 +0100

    PIP

--- Example_11_PIP/ExerciseModel.js
+++ Example_11_PIP/ExerciseModel.js
@@ -19,13 +19,17 @@ var playerRect__ = {
 };
 
 var CurrentTuningParameters;
-var FullScreenTuningParameters = { "ChannelTuningDetails": { "URL": "multicast://227.0.11.1:11022/0/0/0" } };
-var PipTuningParameters = { "ChannelTuningDetails": { "URL": "multicast://229.0.11.1:11022/0/0/0" } };
+var urlFullScreen = "multicast://227.0.11.1:11022/0/0/0";
+var urlPip = "multicast://229.0.11.1:11022/0/0/0";
 
 function ExerciseModelInit() {
 	try {
 		// Register Callback
 		JAPITWIXPPlugin.WebIXPOnReceive = WIXPResponseHandler;
+		if (vidObject.bindToCurrentChannel != undefined) {
+			vidObject.bindToCurrentChannel();
+		}
+		tuneFullscreen();
 	} catch (e) {
 		Logout("exception in ExerciseModelInit()" + e);
 	}
@@ -40,10 +44,8 @@ function ExerciseModelInit() {
 /* this function will call the required function depending on the response received from the TV */
 function WIXPResponseHandler(WIXPResponseJSON) {
 
-	alert("typeof Logout: " + typeof Logout);
 
 	try {
-		Logout("Enter WIXPResponseHandler \n");
 		dataToPass = JSON.parse(WIXPResponseJSON);
 		PrintLogsWIXPFromTV(dataToPass);
 
@@ -58,9 +60,11 @@ function WIXPResponseHandler(WIXPResponseJSON) {
 				} else if (myChannelSelectionStatus == "Successful") {
 
 					// Set Frame for PIP
+					/*
 					if (playerRect__.fChanged == true) {
 						setPipPlayerRect();
 					}
+					*/
 				}
 			}
 
@@ -86,7 +90,6 @@ function WIXPResponseHandler(WIXPResponseJSON) {
 		Logout("WIXPResponseHandler() ERROR. " + e);
 		return e;
 	}
-	//Logout("Exit WIXPResponseHandler \n");
 }
 
 /* function to send commands to TV */
@@ -97,11 +100,11 @@ function sendWIxPCommand(command) {
 		command.Cookie = cookie;
 		var WIXPJSONStringForm = JSON.stringify(command);
 		PrintLogsWIXPToTV(command);
+		Logout("sendWIxPCommand: " + WIXPJSONStringForm);
 		JAPITWIXPPlugin.WebIxpSend(WIXPJSONStringForm);
-		Logout("Exit sendWIxPCommand \n");
 	}
 	catch (e) {
-		Logout("Exit sendWIxPCommand: " + e + " \n");
+		Logout("sendWIxPCommand ERROR. " + e);
 	}
 }
 
@@ -109,30 +112,37 @@ function sendWIxPCommand(command) {
 function CreateJAPITObjectForWIXPSvc() {
 	this.Svc = "WIXP";
 	this.SvcVer = "3.0";
+	this.CommandDetails = {};
 }
 
 
 function tuneFullscreen() {
 	Logout("tuneFullScreen()");
+
 	try {
+		SetPictureSize(-1, -1, -1, -1, "vidDiv");
 		var JAPITObjForWIXPSvc = new CreateJAPITObjectForWIXPSvc();
+		Logout("JAPITObjForWIXPSvc Fullscreen 1: " + JSON.stringify(JAPITObjForWIXPSvc));
 		JAPITObjForWIXPSvc.CmdType = "Change";
 		JAPITObjForWIXPSvc.Fun = "ChannelSelection";
-		JAPITObjForWIXPSvc.CommandDetails = FullScreenTuningParameters;
+		JAPITObjForWIXPSvc.CommandDetails = { "ChannelTuningDetails": { "URL": urlFullScreen } };
+		CurrentTuningParameters = JAPITObjForWIXPSvc.CommandDetails;
 		sendWIxPCommand(JAPITObjForWIXPSvc);
 	} catch (e) {
 		Logout("tuneFullScreen() ERROR. " + e.message);
 	}
-
 }
 
 function tunePIP() {
 	Logout("tunePIP()");
+
 	try {
+		SetPictureSize(435, 480, 427, 240, "vidDiv");
 		var JAPITObjForWIXPSvc = new CreateJAPITObjectForWIXPSvc();
 		JAPITObjForWIXPSvc.CmdType = "Change";
 		JAPITObjForWIXPSvc.Fun = "ChannelSelection";
-		JAPITObjForWIXPSvc.CommandDetails = PipTuningParameters;
+		JAPITObjForWIXPSvc.CommandDetails = { "ChannelTuningDetails": { "URL": urlPip } };
+		CurrentTuningParameters = JAPITObjForWIXPSvc.CommandDetails;
 		sendWIxPCommand(JAPITObjForWIXPSvc);
 	} catch (e) {
 		Logout("tunePIP() ERROR. " + e.message);
@@ -142,49 +152,52 @@ function tunePIP() {
 
 function SetPictureSize(left, top, width, height, element) {
 
-	Logout("SetPictureSize");
-
-	var VideoDiv = document.getElementById(element);
+	Logout("SetPictureSize(" + left + ", " + top + ", " + width + ", " + height + ")");
 
 	try {
-		Logout("SetPictureSize(" + left + ", " + top + ", " + width + ", " + height + ")");
 
-		playerRect__.element = element;
+		stopCurrentChannel();
+		var VideoDiv = document.getElementById(element);
+
+		//playerRect__.element = element;
 
 		if (top == -1 || left == -1 || width == -1 || height == -1) {
 			Logout("set Fullscreen");
 
-			stopCurrentChannel();
-
-			VideoDiv.style.width = "100%";
-			VideoDiv.style.height = "100%";
-			VideoDiv.style.left = "0px";
+			VideoDiv.style.width = "1080px";
+			VideoDiv.style.height = "720px";
+			VideoDiv.style.left = "200px";
 			VideoDiv.style.top = "0px";
 			VideoDiv.style.zIndex = 0;
 
-			Logout('STYLE AFTER CHANGE');
-			Logout(element + '.style.height: ' + this.getStyle(element, "height"));
-			Logout(element + '.style.width: ' + this.getStyle(element, "width"));
-			Logout(element + '.style.top: ' + this.getStyle(element, "top"));
-			Logout(element + '.style.left: ' + this.getStyle(element, "left"));
-			Logout(element + '.style.zIndex: ' + this.getStyle(element, "zIndex"));
+
+
 
 		} else {
 
+			VideoDiv.style.width = width + "px";
+			VideoDiv.style.height = height + "px";
+			VideoDiv.style.left = left + "px";
+			VideoDiv.style.top = top + "px";
+			VideoDiv.style.zIndex = 1000;
+
 			// PIP tuning. resize after succesful tuning
+			/*
 			playerRect__.fChanged = true;
 			playerRect__.style.left = left + "px";
 			playerRect__.style.top = top + "px";
 			playerRect__.style.width = width + "px";
 			playerRect__.style.height = height + "px";
 			playerRect__.style.zIndex = 1000;
-
+			*/
+			/*
 			Logout("playerRect__.left  : " + this.playerRect__.style.left);
 			Logout("playerRect__.top   : " + this.playerRect__.style.top);
 			Logout("playerRect__.width : " + this.playerRect__.style.width);
 			Logout("playerRect__.height: " + this.playerRect__.style.height);
 			Logout("playerRect__.zIndex: " + this.playerRect__.style.zIndex);
-
+			*/
+			/*
 			if (playerRect__.Timer != undefined) {
 				window.clearTimeout(playerRect__.Timer);
 			}
@@ -196,8 +209,15 @@ function SetPictureSize(left, top, width, height, element) {
 					Logout("playerRect__.Timer ERROR." + e.message);
 				}
 			}, 5000);
+			*/
 		}
 
+		Logout('STYLE AFTER CHANGE');
+		Logout(element + '.style.height: ' + getStyle(element, "height"));
+		Logout(element + '.style.width: ' + getStyle(element, "width"));
+		Logout(element + '.style.top: ' + getStyle(element, "top"));
+		Logout(element + '.style.left: ' + getStyle(element, "left"));
+		Logout(element + '.style.zIndex: ' + getStyle(element, "zIndex"));
 
 	} catch (e) {
 		Logout("setPictureSize() ERROR" + e.message);
@@ -208,7 +228,7 @@ function SetPictureSize(left, top, width, height, element) {
 /* set the style for the Player window with the given values in this.playerRect__ */
 function setPipPlayerRect() {
 	Logout("setPlayerRect()");
-	/*
+
 	var element = playerRect__.element;
 	var VideoDiv = document.getElementById(element);
 
@@ -229,7 +249,7 @@ function setPipPlayerRect() {
 		VideoDiv.style.height = playerRect__.style.height;
 		VideoDiv.style.zIndex = playerRect__.style.zIndex;
 
-		vidObject.bindToCurrentChannel();
+
 
 		Logout("setPlayerRect(" + playerRect__.style.left + ", " +
 			playerRect__.style.top + ", " +
@@ -238,28 +258,26 @@ function setPipPlayerRect() {
 			playerRect__.style.zIndex);
 
 		Logout('STYLE AFTER CHANGE');
-		Logout(element + '.style.height: ' + SmartCom.TvPhilipsManager.getStyle(element, "height"));
-		Logout(element + '.style.width: ' + SmartCom.TvPhilipsManager.getStyle(element, "width"));
-		Logout(element + '.style.top: ' + SmartCom.TvPhilipsManager.getStyle(element, "top"));
-		Logout(element + '.style.left: ' + SmartCom.TvPhilipsManager.getStyle(element, "left"));
-		Logout(element + '.style.zIndex: ' + SmartCom.TvPhilipsManager.getStyle(element, "zIndex"));
+		Logout(element + '.style.height: ' + getStyle(element, "height"));
+		Logout(element + '.style.width: ' + getStyle(element, "width"));
+		Logout(element + '.style.top: ' + getStyle(element, "top"));
+		Logout(element + '.style.left: ' + getStyle(element, "left"));
+		Logout(element + '.style.zIndex: ' + getStyle(element, "zIndex"));
 	} catch (e) {
 		this.LogError("setPlayerRect() ERROR#1. " + e.message);
 	}
-	*/
 }
 
 function stopCurrentChannel() {
 
-	Logout("setPlayerRect()");
-	/*
+	Logout("stopCurrentChannel()");
+
 	try {
 		var ParameterObj;
-		document.getElementById(playerRect__.element).style.zIndex = "0";
-
-		if (TuningParameters != undefined) {
-			Logout("stopCurrentChannel()");
-
+		if (playerRect__.element != undefined) {
+			document.getElementById(playerRect__.element).style.zIndex = "0";
+		}
+		if (CurrentTuningParameters != undefined) {
 			var JAPITObjForWIXPSvc = new CreateJAPITObjectForWIXPSvc();
 			JAPITObjForWIXPSvc.CmdType = "Change";
 			JAPITObjForWIXPSvc.Fun = "ChannelSelection";
@@ -271,5 +289,15 @@ function stopCurrentChannel() {
 	} catch (e) {
 		Logout("stopCurrentChannel() ERROR. " + e.message);
 	}
-	*/
+
+}
+
+function getStyle(el, styleProperty) {
+	var myId = document.getElementById(el);
+	var o;
+	if (myId.currentStyle)
+		o = myId.currentStyle[styleProperty];
+	else if (window.getComputedStyle)
+		o = document.defaultView.getComputedStyle(myId, null).getPropertyValue(styleProperty);
+	return o;
 }
